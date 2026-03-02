43364470c508cd0e20e7c3c46dc5fea81bd5ef53
pqnghiep1354 <65300490+pqnghiep1354@users.noreply.github.com>
2026-03-02 09:01:35 +0700

    Delete lib directory

--- lib/apps/routers/router_name.dart
+++
@@ -1,7 +0,0 @@
-class RouterName {
-  static const String splash = '/';
-  static const String home = '/home';
-  static const String cart = '/cart';
-  static const String category = '/category';
-  static const String detail = '/detail';
-}
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'apps/my_app.dart';
-
-void main() {
-  runApp(const MyApp());
-}
